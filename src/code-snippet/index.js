8a43cb5483aaf044b90d75303a4b953a243b6f10
Jake Hickenlooper <jake@weboftomorrow.com>
2018-01-28 11:08:29 -0700

    Use s3 deployment and latest chill

--- src/code-snippet/index.js
+++ src/code-snippet/index.js
@@ -2,6 +2,7 @@
 import 'prismjs'
 import '../../node_modules/prismjs/themes/prism-twilight.css'
 import '../../node_modules/prismjs/components/prism-python.js'
+import '../../node_modules/prismjs/components/prism-sql.js'
 
 // Load the plugins for Prism
 import '../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js'
