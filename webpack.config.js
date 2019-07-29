a05b4441d01bbfcf521332f95e063dea7af37697
Jake Hickenlooper <jake@weboftomorrow.com>
2019-07-29 06:32:08 -0600

    Replace script to clean dist on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -111,7 +112,10 @@ config.module = {
   ]
 };
 
-config.plugins = [new MiniCssExtractPlugin({ filename: "[name].css" })];
+config.plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin({ filename: "[name].css" })
+];
 
 config.stats = "minimal";
 
