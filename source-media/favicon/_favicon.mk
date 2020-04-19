a319e1547bafbb90a7ab9c067e28d24286dad558
Jake Hickenlooper <jake@weboftomorrow.com>
2020-04-19 07:07:01 -0600

    Fix 64x64 favicon typo

--- source-media/favicon/_favicon.mk
+++ source-media/favicon/_favicon.mk
@@ -2,7 +2,7 @@
 
 .PHONY : all clean
 
-small_pngs = source-media/favicon/.favicon-32x32.png source-media/favicon/.favicon-48x48.png source-media/favicon/.favicon-64x46.png
+small_pngs = source-media/favicon/.favicon-32x32.png source-media/favicon/.favicon-48x48.png source-media/favicon/.favicon-64x64.png
 
 objects = $(shell cat source-media/favicon/_favicon.manifest) $(small_pngs)
 
@@ -17,8 +17,8 @@ source-media/favicon/.favicon-32x32.png : media/WoT.svg
 source-media/favicon/.favicon-48x48.png : media/WoT.svg
 	convert +antialias $< -background white -resize 48x48 $@;
 
-source-media/favicon/.favicon-64x46.png : media/WoT.svg
-	convert +antialias $< -background white -resize 64x46 $@;
+source-media/favicon/.favicon-64x64.png : media/WoT.svg
+	convert +antialias $< -background white -resize 64x64 $@;
 
 root/favicon.ico : source-media/favicon/WoT-16x16.png $(small_pngs)
 	convert $^ $@;
